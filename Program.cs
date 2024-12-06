45c72cd8a78dd50a641a5d4718793a1294314931
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-12-06 13:55:07 +0100

    List

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿void Main(){
+    List<double> d = [2, 18, 7.2, 92, 29.2];
+    d.RemoveAt(3);
+
+    double a = Medel(d);
+    Console.WriteLine(a);
+
+}
+
+double Medel( List<double> doubles){
+    double medel = 0;
+    foreach ( double element in doubles){
+        medel += element;
+    }
+
+    medel /= doubles.Count;
+    return medel;
+}
+Main();
